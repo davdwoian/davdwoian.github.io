446a5f7091d2cacacfb6eceec45bdf1b81af537f
woianvms <woianvms@gmail.com>
2022-12-06 05:02:26 +0800

    commit

--- assets/script/encoder.js
+++ assets/script/encoder.js
@@ -1,25 +1,49 @@
-const ints = "0123456789".split('');
-const alphas = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
-const specials = "(~!@#$%^&*_-+=`|\\(){}[]:;\"\'<>,.?/)".split('');
+// @ sha256.min.js
 
-var dm = document.querySelector("#dom");
-var ky = document.querySelector("#key");
-var re = document.querySelector("#res");
+const ints = "0123456789";
+const alps = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const spes = "(~!@#$%^&*_-+=`|\\(){}[]:;\"\'<>,.?/)";
+
+const divide  = (s, i) => s.split(new RegExp(`(.{${i}})`)).filter(o=>o);
+const ascii   = (s) => s.split('').reduce((t, c) => t + c.charCodeAt(0), 0);  
+
+var dm =    document.querySelector("#dom");
+var ky =    document.querySelector("#key");
+var re =    document.querySelector("#res");
 var recpy = document.querySelector("#res-copy");
+var remsk = document.querySelector("#res-mask");
 
-async function encode() {
-    let sha = await sha256(String(dm.value) + String(ky.value));
-
-    let nbs = (await sha256(sha)).split(/(.{8})/).filter(o=>o).map(o=>o.split(/(.{2})/).filter(o=>o).map(x=>ints[(x.charCodeAt(0)+x.charCodeAt(1))%ints.length]));
-    let als = (await sha256((await sha256(sha)))).split(/(.{8})/).filter(o=>o).map(o=>o.split(/(.{2})/).filter(o=>o).map(x=>alphas[(x.charCodeAt(0)+x.charCodeAt(1))%alphas.length]));
-    let sps = (await sha256((await sha256((await sha256(sha)))))).split(/(.{8})/).filter(o=>o).map(o=>o.split(/(.{2})/).filter(o=>o).map(x=>specials[(x.charCodeAt(0)+x.charCodeAt(1))%specials.length]));
-    let grid = [nbs,als,sps];
-
-    re.textContent = sha.split(/(.{4})/).filter(o=>o).map(x=>grid[(((x.charCodeAt(2)+x.charCodeAt(3))%12)/4) >> 0][(x.charCodeAt(0)+x.charCodeAt(1)) % 8][((x.charCodeAt(2)+x.charCodeAt(3))%12)%4]).join('');
+function modifyResTextContent(content) {
+    re.data = content;
+    re.textContent = remsk.data == 'visible' ? content : '*'.repeat(content.length);
 }
 
+async function encode() {
+    let sha0 = await sha256(String(dm.value) + String(ky.value));
+    let sha1 = await sha256(sha0);
+    let sha2 = await sha256(sha1);
+    let sha3 = await sha256(sha2);
 
+    let grid = [
+        divide(sha1, 8).map(o => divide(o, 2).map(x => ints.charAt(ascii(x) % ints.length))),
+        divide(sha2, 8).map(o => divide(o, 2).map(x => alps.charAt(ascii(x) % alps.length))),
+        divide(sha3, 8).map(o => divide(o, 2).map(x => spes.charAt(ascii(x) % spes.length)))
+    ];
+
+    let i = (x) => parseInt(ascii(x.slice(2, 4)) % 12 / 4);
+    let j = (x) => parseInt(ascii(x.slice(0, 2)) % 8);
+    let k = (x) => parseInt(ascii(x.slice(2, 4)) % 4);
+    modifyResTextContent(divide(sha0, 4).map(x => grid[i(x)][j(x)][k(x)]).join(''));
+
+    if (dm.value && ky.value) recpy.click();
+}
 
 dm.addEventListener('change', encode);
 ky.addEventListener('change', encode);
-recpy.addEventListener('click', () => navigator.clipboard.writeText(re.textContent));
+
+recpy.addEventListener('click', () => navigator.clipboard.writeText(re.data));
+remsk.addEventListener('click', () => {
+    remsk.data = remsk.data == 'visible' ? 'invisible' : 'visible';
+    remsk.style['background-image'] = `url(/assets/image/${remsk.data}.png)`;
+    re.textContent = remsk.data == 'visible' ? re.data : '*'.repeat(re.data.length);
+})
